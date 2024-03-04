1049d203df3208f08f48bd318961596c966ed82a
mayurpatil96k <mayur1792001@gmail.com>
2024-03-04 11:36:30 +0530

    .

--- fleeman/src/main/java/com/example/demo/repositories/StateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entities.State;
-
-public interface StateRepository extends JpaRepository<State, Integer>{
-
-}
